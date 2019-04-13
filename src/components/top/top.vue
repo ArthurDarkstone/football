9dc6f3bf31eeaf7885935faf71f416d7fa22e071
LuciferDarkstone <34020340+LuciferDarkstone@users.noreply.github.com>
2019-04-13 16:15:46 +0800

    init
    初始化

---
+++ src/components/top/top.vue
@@ -0,0 +1,65 @@
+<template>
+  <header>
+  	<div class="title">
+      <!-- <i class="back" @click="back" v-if="isBack"><</i> -->
+  	  <i class="iconfont icon-zuqiu"></i>
+      黑科足球
+  	</div>
+    <!-- <transition name="slide"> -->
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      // isBack: this.$store.state.back
+    }
+  },
+  methods: {
+    // back(){
+    //   history.back()
+    // }
+  },
+  mounted() {
+    setTimeout(() => {
+    },20)
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+  .title
+    width: 100%
+    height: 3.2rem
+    line-height: 3.2rem
+    font-size: 1.2rem
+    text-align: center
+    color: $color-w
+    background: $color-g
+    position: relative
+    .back
+      position: absolute
+      top: 0
+      left: 0
+      font-size: 1.5rem
+      padding: 0 .5rem
+      z-index: 20
+      color: #fff
+    .icon-sidebar
+      position: absolute
+      top: 0
+      right: 0
+      font-size: 2rem
+      padding: .6rem .5rem
+      z-index: 20
+      color: #fff
+
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3s ease
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(50%, 0, 0)
+
+</style>
---
+++ src/pages/detail/detail.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="detail">
+    <div class="header">
+      <i class="iconfont icon-fanhui" @click="back" v-if="back"></i>
+      <i class="iconfont icon-zuqiu"></i>
+      黑科足球
+    </div>
+    <iframe :src="url" frameborder="0" name="test" id="dom01" :height="fHeight"></iframe>
+  </div>
+</template>
+
+<script>
+  export default {
+  	date() {
+  		return {
+  		}
+  	},
+    computed: {
+      url() {
+        let id = this.$route.query.id
+        return `http://www.dongqiudi.com/share/article/${id}?id=${id}&type=undefined&refer=m_website`
+      },
+      fHeight() {
+        return window.innerHeight
+      }
+    },
+    methods: {
+      back(){
+        history.back()
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+.detail
+  position: fixed
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  z-index: 100
+  .header
+    width: 100%
+    height: 3.2rem
+    line-height: 3.2rem
+    font-size: 1.2rem
+    text-align: center
+    color: $color-w
+    background: $color-g
+    position: relative
+    color: #fff
+    .icon-fanhui
+      position: absolute
+      top: 0
+      left: 0
+      font-size: 1.5rem
+      padding: .8rem
+      z-index: 20
+      color: #fff
+    .icon-sidebar
+      position: absolute
+      top: .5rem
+      right: 0
+      font-size: 1.5rem
+      padding: 0 .5rem
+      z-index: 20
+      color: #fff
+  iframe
+    width: 100%
+</style>
